74a62d5e71c42be738668221e062e2981c36ae5b
Kenechukwu <kenechukwu.ajufo@gmail.com>
2021-11-09 23:08:08 +0000

    Add Netlify Badge in README

--- src/Components/ModalContent.jsx
+++ src/Components/ModalContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ModalContent(props) {
     return (
     <div>
-    <h4 className="lead">Capital: {props.capital}</h4>
+    <h4 className="lead">Capital: {props.capital ? props.capital : ""}</h4>
     <br/>
     {/*<h4 className="lead">Region: {props.region}</h4>
     <br/>
@@ -15,11 +15,11 @@ export default function ModalContent(props) {
     <br/>
     <h4 className="lead">Landlocked: {(props.landlocked) ? "Yes" : "No"}</h4>
     <br/>*/}
-    <h4 className="lead">Population: {props.population}</h4>
+    <h4 className="lead">Population: {props.population ? props.population : ""}</h4>
     <br/>
-    <h4 className="lead">ISO Code: {props.countryCode}</h4>
+    <h4 className="lead">ISO Code: {props.countryCode ? props.countryCode : ""}</h4>
     <br/>
-    <h4 className="lead">Languages: {Object.values(props.languages).map(value => value + " ")}</h4>
+    <h4 className="lead">Languages: {props.languages ? Object.values(props.languages).map(value => value + " ") : ""}</h4>
     {/*<br/>
     <h4 className="lead">Check the Map Out on <a href={props.maps} target="_blank" rel="noreferrer" > Google Maps</a> </h4>*/}
   </div>
